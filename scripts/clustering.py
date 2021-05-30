095498f384af7985f5f70f248fdbcd5686a13f36
karolwn <karolwn35@gmail.com>
2021-05-30 17:55:44 +0200

    optics and dbscan

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -11,6 +11,7 @@ import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import numpy as np
 import pretty_print
+from sklearn.decomposition import PCA
 import sklearn
 from keras.applications.inception_v3 import InceptionV3
 from keras.applications.resnet50 import ResNet50
@@ -25,12 +26,15 @@ from sklearn.cluster import DBSCAN, OPTICS, KMeans
 from sklearn.metrics import silhouette_samples, silhouette_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import StandardScaler, normalize
 
 os.system("")
 pp = pretty_print.Style()
 
 
 def begin(selected_method, selected_model, selected_dataset):
+    pp.print_red("########## {} {} {}".format(selected_method, selected_model, selected_dataset))
+
     os.chdir(selected_dataset)
 
     pp.print_green('########## initializing model')
@@ -61,15 +65,27 @@ def begin(selected_method, selected_model, selected_dataset):
     filenames = np.array(list(data.keys()))
 
     features = np.array(list(data.values()))
+    features_dimension = features.ndim
 
     pp.print_green('########## reshaping')
     features = np.reshape(features, (features.shape[0], np.prod(features.shape[1:])))
+
+    pp.print_green('########## normalizing')
+    scaler = StandardScaler()
+    features = scaler.fit_transform(features)
+    features = normalize(features)
+
+    pp.print_green('########## pca to 95% variance')
+    pca = PCA(n_components=0.95, svd_solver='full')
+    pca.fit(features)
+    features = pca.transform(features)
+    pp.print_green('########## pca to 95% variance done')
 
     out_path = '../../results/{}/'.format(selected_model)
     pp.print_green('########## clustering')
     start_time = time.time()
 
-    cluster_data(features, filenames, out_path, selected_dataset, selected_method)
+    cluster_data(features, filenames, out_path, selected_dataset, selected_method, features_dimension)
 
     pp.print_green('########## clustering done in {} seconds'.format(time.time() - start_time))
 
@@ -85,11 +101,11 @@ def initialize_model(model_type):
     return model
 
 
-def cluster_data(feat, filenames, results_path, input_path, user_model):
+def cluster_data(feat, filenames, results_path, input_path, user_model, dims):
     if user_model == 'DBSCAN':
-        eps = float(input('max distance to the neighbor: '))
-        min_samples = int(input('number of points to be considered core point:'))
-        method = DBSCAN(eps=eps, min_samples=min_samples, algorithm='auto', metric='minkowski', p=2, n_jobs=-1)
+        pp.print_green('########## calculating neighbors')
+        eps = neighbors_plot(feat)
+        method = DBSCAN(eps=eps, min_samples=(dims * 2), algorithm='auto', metric='minkowski', p=2, n_jobs=-1)
     elif user_model == 'k-means':
         n_clusters = int(input('number of clusters: '))
         random_state = int(input('random state: '))
@@ -97,29 +113,36 @@ def cluster_data(feat, filenames, results_path, input_path, user_model):
     elif user_model == 'knn':
         method = KNeighborsClassifier(n_neighbors=5)
     else:
-        method = OPTICS(min_samples=5, n_jobs=-1)
+        final_xi = 0.004
+        method = OPTICS(min_samples=10, n_jobs=-1, cluster_method='xi', xi=final_xi)
 
     if user_model == 'knn':
         # method.fit(feat, dane_treningowe)
         pass
     else:
+        pp.print_green('########## .fit(data)')
         method.fit(feat)
+        if user_model == 'DBSCAN':
+            plot_dbscan_clusters(feat, method)
+        if user_model == 'OPTICS':
+            make_reachability_plot(method, len(feat), 10)
 
-    groups = {}
-    for file, cluster in zip(filenames, method.labels_):
-        if cluster not in groups.keys():
-            groups[cluster] = []
-            groups[cluster].append(file)
-        else:
-            groups[cluster].append(file)
-
-        try:
-            copyfile(os.path.abspath('{}/{}'.format(input_path, file)),
-                     os.path.abspath('{}/{}/{}'.format(results_path, cluster, file)))
-        except FileNotFoundError:
-            os.mkdir(os.path.abspath('{}/{}/'.format(results_path, cluster)))
-            copyfile(os.path.abspath('{}/{}'.format(input_path, file)),
-                     os.path.abspath('{}/{}/{}'.format(results_path, cluster, file)))
+    pp.print_green('########## saving results to {}'.format(os.path.abspath('{}/'.format(results_path))))
+    # groups = {}
+    # for file, cluster in zip(filenames, method.labels_):
+    #     if cluster not in groups.keys():
+    #         groups[cluster] = []
+    #         groups[cluster].append(file)
+    #     else:
+    #         groups[cluster].append(file)
+    #
+    #     try:
+    #         copyfile(os.path.abspath('{}/{}'.format(input_path, file)),
+    #                  os.path.abspath('{}/{}/{}'.format(results_path, cluster, file)))
+    #     except FileNotFoundError:
+    #         os.mkdir(os.path.abspath('{}/{}/'.format(results_path, cluster)))
+    #         copyfile(os.path.abspath('{}/{}'.format(input_path, file)),
+    #                  os.path.abspath('{}/{}/{}'.format(results_path, cluster, file)))
 
 
 def extract_features(file, model):
@@ -130,3 +153,88 @@ def extract_features(file, model):
     features = model.predict(imgx, use_multiprocessing=True)
 
     return features
+
+
+def neighbors_plot(feat):
+    neighbors_method = NearestNeighbors(n_neighbors=2, metric='minkowski', p=2, n_jobs=-1)
+    neighbors = neighbors_method.fit(feat)
+    distances, indices = neighbors.kneighbors(feat)
+    distances = np.sort(distances, axis=0)
+    distances = distances[:, 1]
+    derivative_dist = np.gradient(distances) / (1/len(distances))
+    half_index = int(len(derivative_dist)/2)
+    border = np.percentile(derivative_dist, 95)
+    item = np.where(derivative_dist > border)[0]
+    try:
+        final_index = item[np.where(item > half_index)][0]
+    except IndexError:
+        border = np.percentile(derivative_dist, 90)
+        item = np.where(derivative_dist > border)[0]
+        final_index = item[np.where(item > half_index)][0]
+    plt.axhline(y=distances[final_index], color='r', linestyle='--')
+    plt.plot(distances)
+    plt.text(0, distances[final_index], str(distances[final_index]))
+    plt.xlabel('sample number')
+    plt.ylabel('distance to the neighbor')
+    plt.title('sorted distances between points')
+    plt.show()
+
+    return distances[final_index]
+
+
+def plot_dbscan_clusters(data, model):
+
+    pca = PCA(n_components=2)
+    pca.fit(data)
+    data_PCA = pca.transform(data)
+
+    labels = model.labels_
+
+    n_clusters_ = len(labels) - (1 if -1 in labels else 0)
+    unique_labels = set(labels)
+    core_samples_mask = np.zeros_like(labels, dtype=bool)
+    core_samples_mask[model.core_sample_indices_] = True
+    colors = [plt.cm.Spectral(each)
+              for each in np.linspace(0, 1, len(unique_labels))]
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = [0, 0, 0, 1]
+
+        class_member_mask = (labels == k)
+
+        xy = data_PCA[class_member_mask & core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], '*', color=tuple(col))
+
+        xy = data_PCA[class_member_mask & ~core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], '.', color=tuple(col))
+
+    plt.title('Clusters, star means core point, black = not clustered')
+    plt.show()
+
+
+def make_reachability_plot(model, data_array_length, dimension):
+
+    space = np.arange(data_array_length)
+    reachability = model.reachability_[model.ordering_]
+    labels = model.labels_[model.ordering_]
+
+    klas = np.unique(labels).tolist()
+
+    for klass in klas:
+        Xk = space[labels == klass]
+        Rk = reachability[labels == klass]
+        if klass == -1:
+            plt.plot(Xk, Rk, 'k.')
+        else:
+            plt.plot(Xk, Rk, '.')
+
+    plt.title('Reachability Plot, min_sample = {}, xi = 0.004, -1 means not clustered'.format(dimension))
+    plt.ylabel('Reachability (epsilon distance)')
+    plt.legend(klas)
+    plt.show()
+
+if __name__ =='__main__':
+    exact_path = '..\\datasets\\geo_shapes\\'
+    exact_path = os.path.abspath(exact_path)
+    begin('OPTICS', 'ResNet50', exact_path)
